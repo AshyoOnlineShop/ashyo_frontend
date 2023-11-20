5a50544e72c8dae673173a6ea57d1c13ac839c81
Jahongirf04 <jahongirf04@gmail.com>
2023-11-20 09:54:33 +0500

    just commit 2

---
+++ src/pages/login/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex items-center justify-center flex-row h-full w-full">
+        <h1 class="font-sans">Login</h1>
+        <input type="text" placeholder="Enter phone number or email">
+        <input type="text" placeholder="Enter passwrod">
+        <button>Login</button>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,12 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {
-        color1: "#12486B",
-        color_bg: "#E7EDF0",
-      },
+        colors: {
+          global_blue: "#134E9B",
+          global_grey: "#EBEFF3",
+          txt_grey: "#545D6A",
+          error_color: "crimson",
+        },
     },
   },
   plugins: [],
