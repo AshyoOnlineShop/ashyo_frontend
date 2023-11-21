a110c661e31f0487aaf7a972753cdd838f44e5b4
Jahongirf04 <jahongirf04@gmail.com>
2023-11-21 10:55:19 +0500

    cart's finished part 2

---
+++ src/components/cart/cart.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="flex flex-row ml-[140px] mt-[60px] gap-[100px]">
+        <div class="flex flex-col gap-[25px]">
+            <h1 class="font-bold text-[30px]">Savat</h1>
+            <div class="flex flex-col gap-[25px]" v-for="(item, index) in items" :key="index">
+                <div class="flex flex-row gap-[70px]">
+                    <img :src="item.img" class="h-[130px] w-[150px] bg-[#d9d9d9] rounded">
+                    <div class="flex flex-col w-[300px] gap-[50px]">
+                        <p class="mt-[7px] text-[gray]">{{ item.category }} {{ item.brand }} {{ item.name }}</p>
+                        <div class="flex gap-[8px] flex-row">
+                            <button class="h-[40px] w-[52px] bg-[#EBEFF3] rounded flex items-center justify-center"><SvgIcon type="mdi" :path="mdiHeartOutline"></SvgIcon></button>
+                            <button @click="del(index)" class="h-[40px] w-[52px] bg-[#EBEFF3] rounded flex items-center justify-center"><SvgIcon type="mdi" :path="mdiTrashCanOutline"></SvgIcon></button>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col gap-[30px]">
+                        <p class="mt-[4px] font-bold flex text-[25px] justify-center items-center">{{ item.price }}<p class="text-[12px] mt-[7px]">&nbsp; UZS</p></p>
+                        <div class="flex flex-row gap-5">
+                            <button :class="item.customer_carts.quantity==1 ? 'pointer-events-none' : ''" class="bg-[#EBEFF3] rounded w-[60px] h-[40px]" @click="dec(index)">-</button>
+                            <p class="text-[24px]">{{ item.customer_carts.quantity }}</p>
+                            <button class="bg-[#EBEFF3] rounded w-[60px] h-[40px]" @click="inc(index)">+</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="w-[340px] h-[325px] rounded bg-[#EBEFF3] mt-[80px] p-[20px] flex flex-col items-center justify-center gap-[30px]">
+            <p class="font-bold text-[20px]">Sizni haridingiz</p>
+            <div class="flex flex-col gap-[20px]">
+                <div class="flex flex-row gap-[130px]">
+                    <p class="text-[#7c7c7c]">Yetkazib berish:</p>
+                    <p class="font-bold">Bepul</p>
+                </div>
+
+                <div class="flex flex-row gap-[35px] items-center">
+                    <p class="text-[#7c7c7c]">Jami summa:</p>
+                    <div class="flex flex-row items-center justify-center gap-[3px]"><p class="font-bold text-[27px]">{{ summa }}</p><p class="font-bold text-[20px] mt-[2px]">uzs</p></div>
+                </div>
+            </div>
+
+            <VButton :btn_type="'primary'">Hoziroq stiib olish</VButton>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiHeartOutline } from '@mdi/js';
+import { mdiTrashCanOutline } from '@mdi/js';
+
+import VButton from '../../components/form/VButton.vue'
+import { ref } from 'vue';
+
+const props : any = defineProps({
+    items: {
+        type: Array(Object)
+    },
+})
+
+let items = ref(props.items)
+
+let summa = ref(0)
+
+const sum = (items: any)=>{
+    for (let i of items){
+        summa.value = summa.value + i.price
+    }
+}
+
+sum(props.items)
+
+const dec = (index: number)=>{
+    
+    items.value[index].customer_carts.quantity = items.value[index].customer_carts.quantity - 1
+}
+
+const inc = (index: number)=>{
+    items.value[index].customer_carts.quantity = items.value[index].customer_carts.quantity + 1
+}
+
+const del = (index: number)=>{
+    summa.value = summa.value - items.value[index].price
+    items.value.splice(index, 1)
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 import customer_signin from "./customer_signin";
 import customer_signup from "./customer_signup";
-import login from "./login";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [login]
-})
+  routes: [
+    customer_signin,
+    customer_signup,
+  ],
+});
 
 export default router;
