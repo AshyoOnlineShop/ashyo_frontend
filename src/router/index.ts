ae09ca009cf0e000ffdbb271aa7787bf07334d5b
Dilnoza Imomova <imomova06@gmail.com>
2023-11-29 08:28:09 +0500

    updated

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,10 +5,6 @@ import main from "./main";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    customer_signin,
-    customer_signup,
-  ],
   routes: [signin, signup, main],
 });
 
